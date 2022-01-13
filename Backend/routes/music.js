291ff75b37bb2e652e727fdfe7d99aff079956e8
Kunal Aggarwal <85010722+Kunal172@users.noreply.github.com>
2022-01-13 19:13:44 +0530

    Delete Backend directory

--- Backend/routes/music.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const musicOperations = require('../controllers/music');
-const songOperations = require('../db/services/songoperations')
-const routes = express.Router();
-routes.get('/singer',musicOperations.getByArtist);
-routes.get('/allsongs',musicOperations.getAllSongs);
-routes.post('/addsong',musicOperations.addASong);
-routes.post('/removesong', songOperations.removeSong);
-module.exports = routes;
